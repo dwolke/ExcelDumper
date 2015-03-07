ae743642ab773895696ef4c6bced92d9a620aee9
Daniel Wolkenhauer <dw@dwolke.de>
2015-03-07 15:48:45 +0100

    initial commit

---
+++ src/ExcelDumper/XlsReader.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ExcelDumper;
+
+/**
+* 
+*/
+class XlsReader
+{
+
+  // function __construct(argument)
+  // {
+  //   # code...
+  // }
+  
+  public static function hello()
+  {
+    return 'Tach Post!';
+  }
+  
+
+}
